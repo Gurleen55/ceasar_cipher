8e12a1dbb3ef36ec22468389f569a8338a2c38e8
Gurleen <gurleensinghdalli@gmail.com>
2024-10-14 12:47:16 -0300

    add test for non-letters

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -10,11 +10,19 @@ describe "#caesar_cipher" do
     end
   end
 
-  context 'when is lowercase letter' do
+  context 'when its lowercase letter' do
     it "shifts them to right by 1" do
       string = 'ef'
       result = caesar_cipher(string, shift)
       expect(result).to eql("fg")
+    end
+  end
+
+  context 'when its anything other than letters' do
+    it "should not change them" do 
+      string = '$7'
+      result = caesar_cipher(string, shift)
+      expect(result).to eql(result)
     end
   end
 end
